07dafda869c00eb7fffd615dc7e690e6b9e55eb4
paulduchesne <paulduchesne@tuta.io>
2023-07-11 12:23:50 +1000

    pull saxon jar

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# fetch saxon-he-12.3.jar.
+if [ ! -f saxon/saxon-he-12.3.jar ]; then
+    echo "hello"
+    mkdir -p saxon
+    cd saxon
+    wget https://github.com/Saxonica/Saxon-HE/releases/download/SaxonHE12-3/SaxonHE12-3J.zip
+    unzip SaxonHE12-3J.zip
+    cd ..
+fi
