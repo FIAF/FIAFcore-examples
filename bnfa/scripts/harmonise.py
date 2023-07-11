bf6d6da21374b1b36d02a41a8b8412efe7dee698
paulduchesne <paulduchesne@tuta.io>
2023-07-11 16:27:31 +1000

    replace countries

--- bnfa/scripts/harmonise.py
+++ bnfa/scripts/harmonise.py
@@ -8,11 +8,36 @@ graph_path = pathlib.Path.cwd() / 'bnfa' / 'rdf' / 'bnfa.ttl'
 graph = rdflib.Graph().parse(graph_path)
 print(len(graph))
 
+
+country_transform = {
+    rdflib.URIRef('http://example.org/EFG/country/BG'): rdflib.URIRef('https://fiafcore.org/ontology/Bulgaria')}
+
+new_graph = rdflib.Graph()
+
 country_property = rdflib.URIRef('https://fiafcore.org/ontology/hasCountry')
 countries = pydash.uniq([o for s,p,o in graph.triples((None, country_property, None))])
-print(countries)
+print(countries) # all countries
+
+for x in countries:
+    if x not in country_transform.keys():
+        raise Exception(x, 'transform not possible.')
+
+
+for s,p,o in graph.triples((None, None, None)):
+    if s in country_transform.keys():
+        s = country_transform[s]
+    if p in country_transform.keys():
+        p = country_transform[p]
+    if o in country_transform.keys():
+        o = country_transform[o]
+
+
+
+    new_graph.add((s,p,o))
+
+
 
 output_path = pathlib.Path.cwd() / 'bnfa' / 'output' / 'bnfa.ttl'
 output_path.parents[0].mkdir(exist_ok=True)
-graph.serialize(destination=str(output_path), format="turtle")
+new_graph.serialize(destination=str(output_path), format="turtle")
 
