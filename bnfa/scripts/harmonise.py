19ded1636422aa404962893aa44ae801f74580bf
paulduchesne <paulduchesne@tuta.io>
2023-07-12 08:32:47 +1000

    rewrote harmonisation

--- bnfa/scripts/harmonise.py
+++ bnfa/scripts/harmonise.py
@@ -1,43 +1,54 @@
 # replace elements to harmonise to shared vocabularies or entity authorities.
 
+import json
 import pathlib
 import pydash
 import rdflib
 
+def check_transform(prop_value, transform_key):
+
+    ''' Check transformation of entities will be possible. '''
+
+    predicate = rdflib.URIRef(f'https://fiafcore.org/ontology/{prop_value}')
+    vocab = pydash.uniq([o for s,p,o in graph.triples((None, predicate, None))])
+    for x in vocab:
+        if str(x) not in transform[transform_key].keys():
+            raise Exception(str(x), 'transform not possible.')
+
+# load transformations.
+
+with open(pathlib.Path.cwd() / 'bnfa' / 'reconcile.json') as transform:
+    transform = json.load(transform)
+    
+# load existing graph.
+
 graph_path = pathlib.Path.cwd() / 'bnfa' / 'rdf' / 'bnfa.ttl'
 graph = rdflib.Graph().parse(graph_path)
-print(len(graph))
 
+# check transformations
 
-country_transform = {
-    rdflib.URIRef('http://example.org/EFG/country/BG'): rdflib.URIRef('https://fiafcore.org/ontology/Bulgaria')}
+check_transform('hasCountry', 'country')
+check_transform('hasForm', 'form')  
+
+# flatten and convert to URIRef.
+
+flat_transform =  {rdflib.URIRef(k): rdflib.URIRef(v) for d in transform for k, v in transform[d].items()}
+
+# find and replace transforms.
 
 new_graph = rdflib.Graph()
 
-country_property = rdflib.URIRef('https://fiafcore.org/ontology/hasCountry')
-countries = pydash.uniq([o for s,p,o in graph.triples((None, country_property, None))])
-print(countries) # all countries
-
-for x in countries:
-    if x not in country_transform.keys():
-        raise Exception(x, 'transform not possible.')
-
-
 for s,p,o in graph.triples((None, None, None)):
-    if s in country_transform.keys():
-        s = country_transform[s]
-    if p in country_transform.keys():
-        p = country_transform[p]
-    if o in country_transform.keys():
-        o = country_transform[o]
-
-
-
+    if s in flat_transform.keys():
+        s = flat_transform[s]
+    if p in flat_transform.keys():
+        p = flat_transform[p]
+    if o in flat_transform.keys():
+        o = flat_transform[o]
     new_graph.add((s,p,o))
 
-
+# save harmonised graph.
 
 output_path = pathlib.Path.cwd() / 'bnfa' / 'output' / 'bnfa.ttl'
 output_path.parents[0].mkdir(exist_ok=True)
 new_graph.serialize(destination=str(output_path), format="turtle")
-
